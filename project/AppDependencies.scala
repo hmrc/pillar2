dac9404aa344db63097a7fa947ed563ea31bb378
Akhilesh Kumar <2045928+AKKUM@users.noreply.github.com>
2022-12-19 09:37:45 +0000

    Intialsetup for Pillar 2 (#2)
    * AK- Fixed some Initail Setup

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,6 +1,7 @@
 import play.core.PlayVersion
 import play.sbt.PlayImport._
 import sbt.Keys.libraryDependencies
+import play.core.PlayVersion.current
 import sbt._
 
 object AppDependencies {
@@ -16,5 +17,14 @@ object AppDependencies {
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % bootstrapVersion            % "test, it",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % hmrcMongoVersion            % Test,
+    "org.mockito"            % "mockito-core"            % "3.7.7"   % "test, it",
+    "com.typesafe.play"      %% "play-test"              % current   % "test, it",
+    "org.pegdown"            % "pegdown"                 % "1.6.0"   % "test, it",
+    "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"   % "test, it",
+    "com.vladsch.flexmark"   % "flexmark-all"            % "0.35.10" % "test, it",
+    "org.scalatestplus"      %% "mockito-3-4"            % "3.2.7.0" % "test",
+    "com.github.tomakehurst" % "wiremock-standalone"     % "2.27.2"  % "it",
+    "com.softwaremill.diffx" %% "diffx-core"             % "0.4.1"   % "test, it",
+    "com.softwaremill.diffx" %% "diffx-scalatest"        % "0.4.1"   % "test, it"
   )
 }
