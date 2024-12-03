30db7516728123261820a947ac5ac54c15369661
JamesMMiller <7511223+JamesMMiller@users.noreply.github.com>
2024-12-03 09:13:46 +0000

    Add connector and controller

--- app/uk/gov/hmrc/pillar2/connectors/UKTaxReturnConnector.scala
+++ app/uk/gov/hmrc/pillar2/connectors/UKTaxReturnConnector.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.pillar2.connectors
 
 import play.api.libs.json._
@@ -9,48 +25,50 @@ import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class UKTaxReturnConnector @Inject()(
-    http: HttpClient,
-    config: AppConfig
+class UKTaxReturnConnector @Inject() (
+  http:        HttpClient,
+  config:      AppConfig
 )(implicit ec: ExecutionContext) {
 
   private def headers(
-      correlationId: Option[String],
-      pillar2Id: Option[String],
-      receiptDate: Option[String],
-      originatingSystem: Option[String],
-      transmittingSystem: Option[String]
+    correlationId:      Option[String],
+    pillar2Id:          Option[String],
+    receiptDate:        Option[String],
+    originatingSystem:  Option[String],
+    transmittingSystem: Option[String]
   ): Seq[(String, String)] = Seq(
-    "correlationid" -> correlationId,
-    "X-Pillar2-Id" -> pillar2Id,
-    "X-Receipt-Date" -> receiptDate,
-    "X-Originating-System" -> originatingSystem,
+    "correlationid"         -> correlationId,
+    "X-Pillar2-Id"          -> pillar2Id,
+    "X-Receipt-Date"        -> receiptDate,
+    "X-Originating-System"  -> originatingSystem,
     "X-Transmitting-System" -> transmittingSystem
   ).collect { case (key, Some(value)) => (key, value) }
 
   def submitUKTaxReturn(
-      payload: JsValue,
-      correlationId: Option[String],
-      pillar2Id: Option[String],
-      receiptDate: Option[String],
-      originatingSystem: Option[String],
-      transmittingSystem: Option[String]
-  )(implicit hc: HeaderCarrier): Future[Either[Result, JsValue]] = {
+    payload:            JsValue,
+    correlationId:      Option[String],
+    pillar2Id:          Option[String],
+    receiptDate:        Option[String],
+    originatingSystem:  Option[String],
+    transmittingSystem: Option[String]
+  )(implicit hc:        HeaderCarrier): Future[Either[Result, JsValue]] = {
     val serviceName = "submit-uk-tax-return"
-    val url = s"${config.baseUrl(serviceName)}"
-    
-    http.POST[JsValue, HttpResponse](
-      url,
-      payload,
-      headers(correlationId, pillar2Id, receiptDate, originatingSystem, transmittingSystem)
-    ).map { response =>
-      response.status match {
-        case 201 => Right(response.json)
-        case 400 => Left(BadRequest(response.json))
-        case 422 => Left(UnprocessableEntity(response.json))
-        case 500 => Left(InternalServerError(response.json))
-        case status => Left(Status(status)(response.json))
+    val url         = s"${config.baseUrl(serviceName)}"
+
+    http
+      .POST[JsValue, HttpResponse](
+        url,
+        payload,
+        headers(correlationId, pillar2Id, receiptDate, originatingSystem, transmittingSystem)
+      )
+      .map { response =>
+        response.status match {
+          case 201    => Right(response.json)
+          case 400    => Left(BadRequest(response.json))
+          case 422    => Left(UnprocessableEntity(response.json))
+          case 500    => Left(InternalServerError(response.json))
+          case status => Left(Status(status)(response.json))
+        }
       }
-    }
   }
-} 
+}
--- app/uk/gov/hmrc/pillar2/controllers/UKTaxReturnController.scala
+++ app/uk/gov/hmrc/pillar2/controllers/UKTaxReturnController.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.pillar2.controllers
 
 import play.api.libs.json._
@@ -9,22 +25,25 @@ import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext
 
 @Singleton
-class UKTaxReturnController @Inject()(
-    cc: ControllerComponents,
-    ukTaxReturnConnector: UKTaxReturnConnector
-)(implicit ec: ExecutionContext) extends BackendController(cc) {
+class UKTaxReturnController @Inject() (
+  cc:                   ControllerComponents,
+  ukTaxReturnConnector: UKTaxReturnConnector
+)(implicit ec:          ExecutionContext)
+    extends BackendController(cc) {
 
   def submitUKTaxReturn(): Action[JsValue] = Action.async(parse.json) { implicit request =>
-    ukTaxReturnConnector.submitUKTaxReturn(
-      request.body,
-      request.headers.get("correlationid"),
-      request.headers.get("X-Pillar2-Id"),
-      request.headers.get("X-Receipt-Date"),
-      request.headers.get("X-Originating-System"),
-      request.headers.get("X-Transmitting-System")
-    ).map {
-      case Right(response) => Ok(response)
-      case Left(error) => error
-    }
+    ukTaxReturnConnector
+      .submitUKTaxReturn(
+        request.body,
+        request.headers.get("correlationid"),
+        request.headers.get("X-Pillar2-Id"),
+        request.headers.get("X-Receipt-Date"),
+        request.headers.get("X-Originating-System"),
+        request.headers.get("X-Transmitting-System")
+      )
+      .map {
+        case Right(response) => Ok(response)
+        case Left(error)     => error
+      }
   }
-} 
+}
