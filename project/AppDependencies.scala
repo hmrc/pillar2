aa77c0e30999ca0b529d7ba23c95e05481c89bd6
JamesMMiller <7511223+JamesMMiller@users.noreply.github.com>
2024-12-06 13:59:41 +0000

    Move wiremock connector tests to it

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -25,13 +25,13 @@ object AppDependencies {
     "org.scalatestplus"      %% "mockito-4-11"             % "3.2.18.0",
     "org.scalatestplus"      %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "wolfendale"             %% "scalacheck-gen-regexp"    % "0.1.2",
-    "com.github.tomakehurst"  % "wiremock-standalone"      % "3.0.1",
     "com.softwaremill.diffx" %% "diffx-core"               % "0.9.0",
     "com.softwaremill.diffx" %% "diffx-scalatest"          % "0.9.0"
   ).map(_ % Test)
 
   val it: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test
+    "com.github.tomakehurst" % "wiremock-standalone"    % "3.0.1",
+    "uk.gov.hmrc"           %% "bootstrap-test-play-30" % bootstrapVersion % Test
   )
 
 }
