fcf0d8ba64cbb0b8c5bf20f29627abd8b180c865
Mohammad Dweik <180322631+MoDweik@users.noreply.github.com>
2024-12-13 16:37:57 +0000

    PIL-1423: Create BTN endpoint and connector (#87)
    * PIL-1423: Create BTN endpoint and connector
    
    * remove redundant requests

---
+++ app/uk/gov/hmrc/pillar2/controllers/BTNController.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.pillar2.controllers
+
+import play.api.Logging
+import play.api.libs.json.Json
+import play.api.mvc.{Action, ControllerComponents}
+import uk.gov.hmrc.pillar2.controllers.actions.{AuthAction, Pillar2HeaderAction}
+import uk.gov.hmrc.pillar2.models.btn.BTNRequest
+import uk.gov.hmrc.pillar2.service.BTNService
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class BTNController @Inject() (
+  btnService:                BTNService,
+  authenticate:              AuthAction,
+  pillar2HeaderExists:       Pillar2HeaderAction,
+  cc:                        ControllerComponents
+)(implicit executionContext: ExecutionContext)
+    extends BackendController(cc)
+    with Logging {
+
+  def submitBtn: Action[BTNRequest] = (authenticate andThen pillar2HeaderExists).async(parse.json[BTNRequest]) { implicit request =>
+    btnService
+      .sendBtn(request.body, request.pillar2Id)
+      .map(response => Ok(Json.toJson(response)))
+  }
+}
