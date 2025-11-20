9ba4b67af9a600c7edc284082ce19531672a1a0b
joshua-lamptey <220714391+joshua-lamptey@users.noreply.github.com>
2025-11-20 16:48:31 +0000

    [PIL-2185] - lower coverage reqs to 90 (still hmrc compliant)

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -16,7 +16,7 @@ object CodeCoverageSettings {
 
   val settings: Seq[Setting[?]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 91,
+    ScoverageKeys.coverageMinimumStmtTotal := 90,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
